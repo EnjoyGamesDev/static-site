c118cbe66b317dd169a47192894c7ea6d2dadb8a
EnjoyGamesDev <46535302+EnjoyGamesDev@users.noreply.github.com>
2025-04-28 18:04:33 -0600

    refactor node_delimiter to  inline_markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -38,3 +38,4 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextType, TextNode
-from node_delimiter import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image
 
 
 class TestNodeDelimiter(unittest.TestCase):
@@ -82,3 +82,21 @@ class TestNodeDelimiter(unittest.TestCase):
             "Broken [link](missing closing"
         )
         self.assertListEqual([], matches)
+        
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
